378415ec76116861e4f99fc314572137da54b139
Hafizh Nurfalah <abduhafizh0@gmail.com>
2025-06-05 16:51:40 +0700

    add search in map

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,8 +1,12 @@
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet'
-import { useState } from 'react'
-import 'leaflet/dist/leaflet.css'
+import { useEffect } from 'react'
+import { useMap } from 'react-leaflet/hooks'
 import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-control-geocoder'
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css'
 
+// Atur ikon default Leaflet
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: '/leaflet/marker-icon-2x.png',
@@ -10,25 +14,58 @@ L.Icon.Default.mergeOptions({
   shadowUrl: '/leaflet/marker-shadow.png',
 })
 
+// Komponen untuk kontrol pencarian lokasi
+function GeocoderControl({ onSelect }) {
+  const map = useMap()
+
+  useEffect(() => {
+    const geocoder = L.Control.geocoder({
+      defaultMarkGeocode: false,
+      placeholder: 'Cari lokasi pengiriman...',
+      geocoder: L.Control.Geocoder.nominatim(),
+      showResultIcons: true,
+    })
+      .on('markgeocode', function (e) {
+        const latlng = e.geocode.center
+        onSelect({ lat: latlng.lat, lng: latlng.lng })
+        map.setView(latlng, 16)
+      })
+      .addTo(map)
+      // Tambahkan ini setelah .addTo(map)
+      L.DomEvent.disableScrollPropagation(geocoder._container)
+      L.DomEvent.disableClickPropagation(geocoder._container)
+
+
+    return () => {
+      map.removeControl(geocoder)
+    }
+  }, [map, onSelect])
+
+  return null
+}
+
+// Komponen untuk memilih lokasi dengan klik pada peta
+function LocationPicker({ onSelect }) {
+  useMapEvents({
+    click(e) {
+      onSelect(e.latlng)
+    },
+  })
+  return null
+}
+
+// Komponen utama peta
 export default function MapComponent({ onSelect, selectedLocation }) {
   const center = { lat: -6.257684, lng: 106.782007 }
 
-  function LocationPicker() {
-    useMapEvents({
-      click(e) {
-        onSelect(e.latlng)
-      }
-    })
-    return null
-  }
-
   return (
-    <MapContainer center={center} zoom={16} style={{ height: '300px', width: '100%' }}>
+    <MapContainer center={center} zoom={13} style={{ height: '300px', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <LocationPicker />
+      <GeocoderControl onSelect={onSelect} />
+      <LocationPicker onSelect={onSelect} />
       {selectedLocation && <Marker position={selectedLocation} />}
     </MapContainer>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "@/styles/globals.css";
 import '@fontsource/plus-jakarta-sans'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-control-geocoder/dist/Control.Geocoder.css'
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
