559e1ba25fd8c511f4ec8da8f43fe1678b650672
Hafizh Nurfalah <abduhafizh0@gmail.com>
2025-06-05 07:18:11 +0700

    fix title

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html>
+      <Head>
+        <title>UPNExpress</title>
+        <link rel="icon" type="image/png" href="/images/logo.png" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
